ebbdea08d2d7a08514fe6787305cc8f226ec7233
Dm7898 <98467453+Dm7898@users.noreply.github.com>
2024-11-02 15:28:00 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3000/api" });
+const API = axios.create({ baseURL: "https://help-desk-mearn-lw6i.vercel.app/api" });
 
 // Adding Authorization header if token exists
 API.interceptors.request.use((config) => {
