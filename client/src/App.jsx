8da0af29564ba9d7d433a92d71af619946046482
Dm7898 <98467453+Dm7898@users.noreply.github.com>
2024-11-03 09:02:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import Register from "./components/Register";
 import Login from "./components/Login";
-// import TicketList from "./components/TicketList";
 import TicketDetail from "./components/TicketDetail";
 import TicketsPage from "./pages/TicketsPage";
 import Sidebar from "./components/Sidebar";
@@ -9,14 +9,33 @@ import Home from "./pages/Home";
 import Customers from "./components/Customers";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <Router>
       <div className="flex flex-col lg:flex-row">
-        {/* Sidebar - full width on small screens, 30% on large screens */}
-        <Sidebar />
+        {/* Mobile toggle button */}
+        <button
+          onClick={toggleSidebar}
+          className="lg:hidden p-4 text-white bg-blue-500 w-full"
+        >
+          Menu
+        </button>
 
-        {/* Main content - full width on small screens, 70% on large screens */}
-        <div className="w-full lg:w-3/4 flex justify-center items-center">
+        {/* Sidebar */}
+        <div
+          className={`${
+            isSidebarOpen ? "block" : "hidden"
+          } lg:block lg:w-1/4 bg-gray-100`}
+        >
+          <Sidebar />
+        </div>
+
+        {/* Main content */}
+        <div className="w-full lg:w-3/4 flex justify-center items-center p-4">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
